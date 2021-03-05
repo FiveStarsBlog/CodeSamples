9042832025822d55ff73860508cd98c193806802
Federico Zanetello <zntfdr@gmail.com>
2021-03-05 21:40:34 +0700

    add app state snippet

---
+++ App-State-In-SwiftUI/AppState.swift
@@ -0,0 +1,68 @@
+// Gist from https://fivestars.blog/swiftui/app-state.html
+
+import SwiftUI
+
+@main
+struct FiveStarsApp: App {
+  @StateObject var appStateContainer = AppStateContainer()
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .environmentObject(appStateContainer)
+        .environmentObject(appStateContainer.tabViewState)
+    }
+  }
+}
+
+enum Tab: Hashable {
+  case home
+  case settings
+}
+
+class TabViewState: ObservableObject {
+  @Published var selectedTab: Tab = .home
+}
+
+class AppStateContainer: ObservableObject {
+  var tabViewState = TabViewState()
+}
+
+struct ContentView: View {
+  @EnvironmentObject var state: TabViewState
+
+  var body: some View {
+    TabView(selection: $state.selectedTab) {
+      HomeView()
+        .tabItem {
+          Label("Home", systemImage: "house.fill")
+        }
+        .tag(Tab.home)
+
+      SettingsView()
+        .tabItem {
+          Label("Settings", systemImage: "gear")
+        }
+        .tag(Tab.settings)
+    }
+  }
+}
+
+struct HomeView: View {
+  @EnvironmentObject var state: AppStateContainer
+
+  var body: some View {
+    VStack {
+      Button("go to settings") {
+        state.tabViewState.selectedTab = .settings
+      }
+      Text("Home")
+    }
+  }
+}
+
+struct SettingsView: View {
+  var body: some View {
+    Text("Settings")
+  }
+}
---
+++ App-State-In-SwiftUI/README.md
@@ -0,0 +1,3 @@
+Code snippet from [App-wise state in SwiftUI][fs].
+
+[fs]: https://fivestars.blog/swiftui/app-state.html
