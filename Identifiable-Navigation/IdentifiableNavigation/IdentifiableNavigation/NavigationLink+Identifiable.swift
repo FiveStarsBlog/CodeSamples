fc4764c16c349d5e712d0a276a0b011c4060ff38
Federico Zanetello <zntfdr@gmail.com>
2021-12-09 12:12:06 +0700

    add fix for Xcode 12.5 and later

--- Identifiable-Navigation/IdentifiableNavigation/IdentifiableNavigation/NavigationLink+Identifiable.swift
+++ Identifiable-Navigation/IdentifiableNavigation/IdentifiableNavigation/NavigationLink+Identifiable.swift
@@ -2,28 +2,32 @@
 
 import SwiftUI
 
-extension NavigationLink where Label == EmptyView {
-  public init?<V: Identifiable>(
+extension NavigationLink where Label == EmptyView, Destination == AnyView {
+  public init<V: Identifiable, Destination2: View>(
     item: Binding<V?>,
-    destination: @escaping (V) -> Destination
+    destination: @escaping (V) -> Destination2
   ) {
-    if let value = item.wrappedValue {
-      let isActive: Binding<Bool> = Binding(
-        get: { item.wrappedValue != nil },
-        set: { value in
-          if !value {
-            item.wrappedValue = nil
-          }
+    let value: V? = item.wrappedValue
+    let isActive: Binding<Bool> = Binding(
+      get: { item.wrappedValue != nil },
+      set: { value in
+        if !value {
+          item.wrappedValue = nil
         }
-      )
+      }
+    )
 
-      self.init(
-        destination: destination(value),
-        isActive: isActive,
-        label: EmptyView.init
-      )
-    } else {
-      return nil
-    }
+    self.init(
+      destination:
+        AnyView(Group {
+          if let value = value {
+            destination(value)
+          } else {
+            EmptyView()
+          }
+        }),
+      isActive: isActive,
+      label: EmptyView.init
+    )
   }
 }
